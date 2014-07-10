39c4410680a520336bebc25c8e50ffc8acdbd646
Enno Boland <g@s01.de>
2014-07-10 11:22:50 +0200

    initial commit

---
+++ vnc2gif.js
@@ -0,0 +1,60 @@
+/*
+ * vnc2gif.js
+ * Copyright (C) 2014 tox <tox@rootkit>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+var rfb = require('rfb2')
+var Canvas = require("canvas");
+var GIFEncoder = require('gifencoder');
+var async = require('async');
+var connect = require('connect');
+var http = require('http');
+var morgan = require('morgan');
+var serveStatic = require('serve-static');
+var qs = require('qs');
+
+function handleConnection(req, res, next) {
+	var query = qs.parse(req.url)
+	var args = {
+		host: query.host || 'localhost',
+		port: query.port ? parseInt(params.query.port,10) : 5900,
+		password: query.password || ''
+	};
+	var r = rfb.createConnection(args);
+
+	var canvas;
+	var gif;
+	var ctx;
+
+	r.on('connect', function() {
+		res.writeHead(200, { 'Content-Type': 'image/gif' });
+		canvas = new Canvas(r.width, r.height);
+		gif = new GIFEncoder(r.width, r.height);
+		gif.start();
+		gif.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+		gif.setDelay(500);  // frame delay in ms
+		gif.setQuality(10);
+		ctx = canvas.getContext('2d');
+		gif.createReadStream().pipe(res);
+		console.log('connect')
+	})
+	.on('rect', function(rect) {
+		var imgData = ctx.createImageData(rect.width, rect.height);
+		for(var i = 0; i < rect.data.length; i++) {
+			imgData.data[i] = rect.data[i];
+		}
+		console.log('rect');
+		ctx.putImageData(imgData, rect.x, rect.y);
+		gif.addFrame(ctx);
+		r.requestUpdate(false, 0, 0, r.width, r.height);
+	})
+}
+
+var app = connect()
+	.use(morgan('dev'))
+	.use(serveStatic('public'))
+	.use(handleConnection)
+
+http.createServer(app).listen(3000);
